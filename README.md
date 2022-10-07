dbc9f4e225d85e74ff5e40f20243e38f59469122
Regina Renzy Buban <renzy.buban@tell.com>
2022-10-07 23:19:15 +0800

    add readme and fix homepage

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # League
+
+## GitHub
+```
+git clone https://github.com/renzyyyyy/league
+``` 
+## Docker
+* This can be built using docker
+```
+docker-compose up --build -d
+```
+* Follow the bash commands. Make sure to run the php artisan commands in the container terminal
+* The site will be available in http://localhost:8088/
+## Bash
+* navigate to project folder /league
+* navigate to /src and run
+```
+composer install
+```
+* create your .env file and edit DB_ settings.
+* You may need to generate app key
+```
+php artisan key:generate
+```
+* We need to build the database
+```
+php artisan migrate
+```
+* Run the seeders
+```
+php artisan db:seed --class=RolesSeeder
+php artisan db:seed --class=ChampionsSeeder
+```
+
--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -83,16 +83,9 @@
                 <div class="title m-b-md">
                     League
                 </div>
-                {{ route('champions.create') }}
+                
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('champions.index') }}">Champions</a>
                 </div>
             </div>
         </div>
